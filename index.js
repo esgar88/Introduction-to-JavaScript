8ddbe4d31aa8b122669e8f5888debf8150234a4d
Estevan Garcia <estevangarcia@MillennumFalcon.attlocal.net>
2021-02-16 20:15:50 -0600

    completed

--- index.js
+++ index.js
@@ -144,7 +144,21 @@ Use the game function below to do the following:
 */
 
 function game(user, computer) {
-	/*add your code here*/
+	if (user === computer) {
+		return "it's a tie";
+	} else if (user === "paper" && computer === "rock") {
+		return "you win!";
+	} else if (user === "paper" && computer === "scissors") {
+		return "you lose!";
+	} else if (user === "rock" && computer === "paper") {
+		return "you lose!";
+	} else if (user === "rock" && computer === "scissors") {
+		return "you win!";
+	} else if (user === "scissors" && computer === "rock") {
+		return "you lose!";
+	} else if (user === "scissors" && computer === "paper") {
+		return "you win!";
+	}
 }
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -158,9 +172,10 @@ Using the miles function below do the following:
   3. Return the number of miles
 */
 
-function miles(/*add your code here*/) {
-	/*add your code here*/
+function miles(kilometers) {
+	return kilometers * 0.621371;
 }
+console.log(miles(10));
 
 //Task 5b - Feet to CM
 /*
@@ -170,8 +185,8 @@ Using the feet function below do the following:
   3. Return number of feet
 */
 
-function feet(/*add your code here*/) {
-	/*add your code here*/
+function feet(cm) {
+	return cm / 30.48;
 }
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
@@ -184,8 +199,10 @@ Using the annoyingSong function below do the following:
       "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(/*add your code here*/) {
-	/*add your code here*/
+function annoyingSong(number) {
+	for (let i = number; i > 0; i--) {
+		return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i - 1} bottles of soda on the wall`;
+	}
 }
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 7 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
